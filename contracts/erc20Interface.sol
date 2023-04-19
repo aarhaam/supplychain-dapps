ed12af737038255b176f9ffdda66d3b26ca42bd3
dika <muhammadkharismam@gmail.com>
2023-04-19 13:23:18 +0800

    erc20 token init

--- contracts/erc20Interface.sol
+++ contracts/erc20Interface.sol
@@ -8,6 +8,7 @@ contract ERC20Interface {
   function balanceOf(address tokenOwner) public view returns (uint balance);
   function allowance(address tokenOwner, address spender) public view returns (uint remaining);
   function transfer(address to, uint tokens) public returns (bool success);
+  function approve(address spender, uint tokens) public returns (bool success);
   function transferFrom(address from, address to, uint tokens) public returns (bool success);
 
   event Transfer(address indexed from, address indexed to, uint tokens);
--- contracts/erc20Token.sol
+++ contracts/erc20Token.sol
@@ -35,4 +35,34 @@ contract ERC20Token is ERC20Interface {
     return true;
   }
 
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+    uint256 allowance = allowed[_from][msg.sender];
+    require(balances[_from] >= _value && allowance >= _value, "Insufficient allowed funds for transfer source.");
+    balances[_to] += _value;
+    balances[_from] -= _value;
+    if (allowance < MAX_UINT256) {
+      allowed[_from][msg.sender] -= _value;
+    }
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool success) {
+    allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+  function totalSupply() public view returns (uint256 totSupp) {
+    return totSupply;
+  }
+
 }
